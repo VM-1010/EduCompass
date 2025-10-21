6314bd597e839c63c5f0e1ea212614627ab00842
VM-1010 <vishnumohanan9866@gmail.com>
2025-10-21 17:32:32 +0530

    end basic gui

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,12 +1,12 @@
 package controllers;
-
+//  for both admin and user views
 import javafx.scene.control.Button;
 import javafx.event.*;
 
 import javafx.fxml.*;
 
 public class AdminController {
-    @FXML private Button backbtn;
+    @FXML private Button backbtn, ubackbtn;
 
     @FXML private void whenBackBtnPressed(ActionEvent e) throws Exception{
         gui.App.getPrimaryStage().getScene().setRoot(new FXMLLoader(getClass().getResource("../resources/mainmenu.fxml")).load());
